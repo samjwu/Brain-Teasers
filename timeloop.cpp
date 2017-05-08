6385e3dc1997de3972f11de7ddb4a0c3143991b9
cmput274 <sjwu@ualberta.ca>
2017-05-07 21:13:52 -0600

    timeloop

---
+++ timeloop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  int numlines;
+  std::cin >> numlines;
+
+  for (int i = 1; i <= numlines; i++) {
+    std::cout << i << " Abracadabra" << std::endl;
+  }
+
+  return 0;
+}
---
+++ timeloop.py
@@ -0,0 +1,4 @@
+numlines = int(input())
+
+for i in range(0, numlines):
+    print(i+1, "Abracadabra")
